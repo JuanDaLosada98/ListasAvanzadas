c69c6963120ec3550d6d5def411f2e2eea3473b5
estudiante <juandavidlosada2@gamil.com>
2018-08-30 13:04:52 -0500

    yiiiiiju

--- app/src/main/java/com/example/estudiante/listasavanzadas/MainActivity.java
+++ app/src/main/java/com/example/estudiante/listasavanzadas/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView lv_noticias;
     NoticiaAdapter customAdapter;
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
